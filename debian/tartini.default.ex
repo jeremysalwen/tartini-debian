aa71c42855efd050cfc21f78e8b0fe59835247c2
Jeremy Salwen <jeremysalwen@gmail.com>
2011-05-25 19:13:27 -0400

    Added debian dir.

---
+++ debian/tartini.default.ex
@@ -0,0 +1,10 @@
+# Defaults for tartini initscript
+# sourced by /etc/init.d/tartini
+# installed at /etc/default/tartini by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
